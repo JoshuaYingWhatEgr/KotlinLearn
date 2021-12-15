1ad94219c2237f8b54c4c8ef86a16acd92522d8a
joshuayingwhat <joshuayingwhat@163.com>
2021-12-15 11:30:03 +0800

    run 使用方法

---
+++ src/com/joshauyingwhat/learn/RunFun.kt
@@ -0,0 +1,25 @@
+package com.joshauyingwhat.learn
+
+/**
+ * run 内置函数
+ */
+fun main() {
+
+    val str = "joshuayingwhat"
+
+    println(str.run(::isLong))
+
+    println(str.run(::isLong)
+            .run(::showText))
+
+    println(str.let(::isLong)
+            .let(::showText))
+}
+
+fun isLong(str: String?): Boolean {
+    return str?.let { it.length > 5 } ?: false
+}
+
+fun showText(isLong:Boolean):String {
+    return if(isLong) "show text" else "false"
+}
